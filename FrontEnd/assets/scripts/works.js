6fdfe48a862ae599601e74d9fb0171415c042aea
Sodany Sean <champloo63@gmail.com>
2024-01-26 13:02:51 +0100

    Ajout des boutons et galerie dynamiques

--- FrontEnd/assets/scripts/works.js
+++ FrontEnd/assets/scripts/works.js
@@ -1,29 +1,82 @@
 
 
+let allWorks = []
+let allCategories = []
+///////////// FUSION DES FONCTIONS A AFFICHER/////////////////
+function fetchAll() {
+  fetchCategory();
+  fetchGallery();
+}
+function fetchCategory() {
+  fetch("http://localhost:5678/api/categories")
+  .then(response => response.json())
+  .then(data => {
+    // Afficher toutes les boutons
+    allCategories = data
+    renderCategories(data)
+    console.table(data)
+  })
+  .catch(error => console.error('Erreur lors de la récupération des images:', error));
 
-// Récupération des travaux sur API via Fetch
-fetch("http://localhost:5678/api/works")
-.then(response => response.json())
-.then (works => {
-    console.table(works)
+}
+function renderCategories(categories) {
+  categories.forEach(category => {
+  const divButtons = document.querySelector(".filters")
+  const categoryButton = document.createElement("button")
 
-    for (let i = 0; i < works.length; i++) {
+  categoryButton.innerText = category.name
+  categoryButton.setAttribute("class", "category-button")
+  categoryButton.setAttribute("data-id", category.id)
 
-    const card = works[i]
+  divButtons.appendChild(categoryButton)
 
-    const figureFiche = document.querySelector(".gallery")
-    const createFigure = document.createElement("figure")
-    const imageWorks = document.createElement("img")
-    imageWorks.src = card.imageUrl
-    const titleWorks = document.createElement("figcaption")
-    titleWorks.innerText = card.title
-    
+  })
+}
+function fetchGallery() {
+    fetch("http://localhost:5678/api/works")
+      .then(response => response.json())
+      .then(data => {
+        // Afficher toutes les images
+        allWorks = data
+        renderGallery(data)
+        console.table(data)
+      })
+      .catch(error => console.error('Erreur lors de la récupération des images:', error));
+}
+function renderGallery(works) {
 
-    figureFiche.appendChild(createFigure)
-    createFigure.appendChild(imageWorks)
-    createFigure.appendChild(titleWorks)
-    }
+    document.querySelector(".gallery").innerHTML=""
 
-})
+    works.forEach(work => {
+      const divGallery = document.querySelector(".gallery")
+      const figureWorks = document.createElement("figure")
+      const imageWorks = document.createElement("img")
+      const titleWorks = document.createElement("figcaption")
+      // Définition de chaque élément
+      imageWorks.src = work.imageUrl
+      imageWorks.alt = work.title
+      titleWorks.innerText = work.title
+      // Création des enfants
+      divGallery.appendChild(figureWorks)
+      figureWorks.appendChild(imageWorks)
+      figureWorks.appendChild(titleWorks) 
+      })
+
+      // Ajout d'un gestionnaire d'événements au bouton
+      const filterButtons = document.querySelectorAll(".category-button")
+      filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
+          const valueIdButton = parseInt(button.getAttribute("data-id"))
+          console.log(valueIdButton)
+          const filtered = allWorks.filter((work) => work.categoryId === valueIdButton)
+          console.table(filtered)
+          // Vider la page avant d'afficher le filtre
+          document.querySelector(".gallery").innerHTML=""
+          renderGallery(filtered)
+        })
+      })
+}
+
+fetchAll()
 
 
